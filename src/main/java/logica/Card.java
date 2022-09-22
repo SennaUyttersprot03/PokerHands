e2142dee6e4c413bdb1f1fb8f76dc30ed4602c8e
LeanderBaert <79011657+LeanderBaert@users.noreply.github.com>
2022-09-22 16:30:19 +0200

    Initial commit

--- src/main/java/logica/Card.java
+++ src/main/java/logica/Card.java
@@ -1,5 +1,6 @@
 package logica;
 import Enum.CardTypes;
+
 /**
  * PokerHands : Card
  *
--- src/main/java/logica/PokerHand.java
+++ src/main/java/logica/PokerHand.java
@@ -7,5 +7,14 @@ package logica;
  * @version 22/09/2022
  */
 public class PokerHand {
+    private Card[] hand = new Card[5];
+
+    public PokerHand(Card hand[]){
+        if(hand.length > 5){
+            throw new IllegalArgumentException("Hand is groter dan 5");
+        }else if(hand.length < 5){
+            throw new IllegalArgumentException("Hand is kleiner dan 5");
+        }
+    }
 
 }
